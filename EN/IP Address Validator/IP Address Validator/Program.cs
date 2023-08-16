5e8b4b87fed20692937d0f4d18b0ec0042099c99
delunatriestocode <delunacomunicacao@gmail.com>
2023-08-15 22:50:34 -0300

    Adicionar arquivos de projeto.

---
+++ EN/IP Address Validator/IP Address Validator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IP_Address_Validator {
+    internal class Program {
+        static void Main(string[] args) {
+            String ip = "";
+            String[] octets = new String[4];
+            int dotCounter = 0;
+            Boolean octetIsValid = false;
+
+            //User input
+            Console.Write("Insira um endereço de IP: ");
+            ip = Console.ReadLine();
+            //Count how many dots the IP address have
+            for (int i = 0; i < ip.Length; i++) {
+                if (ip[i] == '.') {
+                    dotCounter++;
+                }
+            }
+            //Create the octet array
+            octets = ip.Split('.');
+            //Check if the IP address have 4 octets
+            if (octets.Length == 4) {
+                //Check if every octet between 0 and 255
+                if ((Convert.ToInt32(octets[0]) >= 0 && Convert.ToInt32(octets[0]) <= 255) &&
+                    (Convert.ToInt32(octets[1]) >= 0 && Convert.ToInt32(octets[1]) <= 255) &&
+                    (Convert.ToInt32(octets[2]) >= 0 && Convert.ToInt32(octets[2]) <= 255) &&
+                    (Convert.ToInt32(octets[3]) >= 0 && Convert.ToInt32(octets[3]) <= 255)) {
+                    octetIsValid = true;
+                }
+                else {
+                    Console.WriteLine("INVALID IP: All octets need to have a value between 0 and 255.");
+                    octetIsValid = false;
+                }
+            }
+            else {
+                Console.WriteLine("INVALID IP: The IP Address need to have 4 octets.");
+                octetIsValid = false;
+            }
+            if (dotCounter == 3 && octetIsValid) {
+                Console.WriteLine($"The IP {ip} is a valid address!", ip);
+            }
+        }
+    }
+}
---
+++ PTBR/Validador de IP/Validador de IP/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Validador_de_IP {
+    internal class Program {
+        static void Main(string[] args) {
+            String ip = "";
+            String[] octetos = new String[4];
+            int contadorDePontos = 0;
+            Boolean octetoEhValido = false;
+
+            //Input do usuário
+            Console.Write("Insira um endereço de IP: ");
+            ip = Console.ReadLine();
+            //Contar os pontos no endereço de IP inserido
+            for (int i = 0; i < ip.Length; i++) { 
+                if (ip[i] == '.') {
+                    contadorDePontos++;
+                }
+            }
+            //Criar um array de octetos a partir do endereço de IP
+            octetos = ip.Split('.');
+            //Checar se o endereço de IP tem quatro octetos
+            if (octetos.Length == 4) {
+                //Checar se todos os octetos são maiores ou iguais a 0 e menores ou iguais a 255
+                if ((Convert.ToInt32(octetos[0]) >= 0 && Convert.ToInt32(octetos[0]) <= 255) && 
+                    (Convert.ToInt32(octetos[1]) >= 0 && Convert.ToInt32(octetos[1]) <= 255) && 
+                    (Convert.ToInt32(octetos[2]) >= 0 && Convert.ToInt32(octetos[2]) <= 255) && 
+                    (Convert.ToInt32(octetos[3]) >= 0 && Convert.ToInt32(octetos[3]) <= 255)) {
+                    octetoEhValido = true;
+                } else {
+                    Console.WriteLine("IP INVÁLIDO: Todos os octetos devem estar entre 0 e 255.");
+                    octetoEhValido = false;
+                } 
+            } else {
+                Console.WriteLine("IP INVÁLIDO: O endereço de IP deve ter quatro octetos.");
+                octetoEhValido = false;
+            }
+            if (contadorDePontos == 3 && octetoEhValido) {
+                Console.WriteLine($"O IP {ip} é um endereço válido!", ip);
+            } 
+        }   
+    }
+}
