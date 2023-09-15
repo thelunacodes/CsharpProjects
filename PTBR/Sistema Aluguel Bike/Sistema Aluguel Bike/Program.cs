2356263d6635a988aafc9cc71bd365b3f0c5f506
delunacomunicacao@gmail.com <delunacomunicacao@gmail.com>
2023-09-15 10:38:57 -0300

    csv + métodos

--- PTBR/Sistema Aluguel Bike/Sistema Aluguel Bike/Program.cs
+++ PTBR/Sistema Aluguel Bike/Sistema Aluguel Bike/Program.cs
@@ -1,27 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading;
 
 namespace Sistema_Aluguel_Bike {
     internal class Program {
+        static int opcao0 = 0, opcao1 = 0, opcao2 = 0, opcao3 = 0, opcao4 = 0, opcao4_b;
+        static string modeloBike = "", tamanhoBike = "", corBike = "";
+        static string opcaoInvalidaString = "ERRO: Opção inválida!";
+        static string listaVaziaString = "ERRO: Lista vazia!";
+        const string NOME_ARQUIVO = "bikes.csv";
+        const string DIRETORIO = @"C:\Users\gabriel.luna\Desktop";
+        static string caminho = Path.Combine(DIRETORIO, NOME_ARQUIVO);
+        static bool disponivelBike;
+        static double valAluguelBike, valDepositoBike;
+        static List<Bicicleta> listaBicicletas = new List<Bicicleta>();
+        static List<Cliente> listaClientes = new List<Cliente>();
+        static List<Funcionario> listaFuncionarios = new List<Funcionario>();
+        static List<string> listaLinhas = new List<string>();
         static void Main(string[] args) {
-            int opcao0 = 0;
-            int opcao1 = 0;
-            int opcao2 = 0;
-            int opcao3 = 0;
-            int opcao4 = 0, opcao4_B = 0;
-            string modeloBike = "", tamanhoBike = "", corBike = "";
-            string opcaoInvalidaString = "ERRO: Opção inválida!";
-            string listaVaziaString = "ERRO: Lista vazia!";
-            bool disponivelBike;
-            double valAluguelBike, valDepositoBike;
-            List<Bicicleta> listaBicicletas = new List<Bicicleta>();
-            List<Cliente> listaClientes = new List<Cliente>();
-            List<Funcionario> listaFuncionarios = new List<Funcionario>();
-
-            //Placeholder Bike
-            listaBicicletas.Add(new Bicicleta(listaBicicletas.Count, "modelo", "tamanho", "cor", 420.00, 69.00, true));
-
+            LerArquivo();
             do {
                 switch (opcao0) {
                     case 0:
@@ -70,69 +69,8 @@ namespace Sistema_Aluguel_Bike {
                         //ALUGAR BIKE
                         break;
                     case 4:
-                        //LISTAR BIKES
-                        if (listaBicicletas.Count == 0) {
-                            Console.WriteLine(listaVaziaString);
-                            opcao0 = 0;
-                        } else {
-                            int minIndex = 0;
-                            int maxIndex = 4;
-                            int minPage = (maxIndex + 1) / 5;
-                            int maxPage = Convert.ToInt32(Math.Round(Convert.ToDouble(listaBicicletas.Count) / 5 ) > 0 ? Math.Round(Convert.ToDouble(listaBicicletas.Count) + 1) : 1);
-                            int index = 0;
-
-                            do {
-                                //INTRO + EXIBIR ITENS + OPÇÕES
-                                Console.WriteLine("\n===================================\n\tLista de Bicicletas\n===================================\n");
-                                Console.WriteLine($"Página {minPage}/{maxPage}:\n");
-                                for (int i = 0; i < listaBicicletas.Count; i++) {
-                                    Console.Write($"[{listaBicicletas[i].Id}] - {listaBicicletas[i].Modelo} {listaBicicletas[i].Tamanho} ");
-                                    if (listaBicicletas[i].Disponivel) {
-                                        Console.Write("(Disponível)");
-                                    } else {
-                                        Console.Write("(Indisponível)");
-                                    }
-                                    Console.WriteLine();
-                                }
-                                Console.WriteLine("\n\nInsira o número correspondente à opção desejada: \n");
-                                Console.WriteLine("[1] Acessar informações de uma bicicleta pelo ID \n[2] Remover bicicleta da lista \n[4] Página anterior \n[5] Próxima página \n[6] Sair");
-                                do {
-                                    opcao4 = InputInt("\n> ");
-                                    if (opcao4 < 1 || opcao4 > 6) {
-                                        Console.WriteLine(opcaoInvalidaString);
-                                    } else {
-                                        break;
-                                    }
-                                } while (true);
-                                //ACESSAR INFORMAÇÕES PELO ID
-                                if (opcao4 == 1) {
-                                    opcao4_B = InputInt("\nInsira o ID da bicicleta: ");
-                                    if (opcao4_B < 0 || opcao4_B > listaBicicletas.Count - 1) {
-                                        Console.WriteLine(opcaoInvalidaString);
-                                    }
-                                    else {
-                                        Console.WriteLine("\nID: " + listaBicicletas[opcao4_B].Id);
-                                        Console.WriteLine("Modelo: " + listaBicicletas[opcao4_B].Modelo);
-                                        Console.WriteLine("Tamanho: " + listaBicicletas[opcao4_B].Tamanho);
-                                        Console.WriteLine("Cor: " + listaBicicletas[opcao4_B].Cor);
-                                        Console.WriteLine("Aluguel: R$ " + String.Format("{0:0.00}", listaBicicletas[opcao4_B].ValAluguel));
-                                        Console.WriteLine("Depósito: R$ " + String.Format("{0:0.00}", listaBicicletas[opcao4_B].ValDeposito));
-                                        if (listaBicicletas[opcao4_B].Disponivel) {
-                                            Console.WriteLine("Disponível: sim");
-                                        }
-                                        else {
-                                            Console.WriteLine("Disponível: não");
-                                        }
-                                    }
-                                    Thread.Sleep(2500);
-                                } else if (opcao4 == 2) {
-                                    
-                                } else if (opcao4 == 6) {
-                                    opcao0 = 0;
-                                    break;
-                                } 
-                            } while (true);
-                        }
+                        ListarBikes();
+                        opcao0 = 0;
                         break;
                     case 5:
                         //LISTAR FUNCIONÁRIO
@@ -212,6 +150,113 @@ namespace Sistema_Aluguel_Bike {
                 }
             } while (true);
             return atributo;
+        }
+        //Ler arquivo csv
+        public static void LerArquivo() {
+            if (!File.Exists(caminho)) {
+                Console.WriteLine("ERRO: O arquivo não existe!");
+            }
+            try {
+                using (StreamReader reader = new StreamReader(caminho)) {
+                    string str = reader.ReadLine();
+                    while (true) {
+                        if (str == null) {
+                            break;
+                        }
+                        char[] separator = new char[] { ';' };
+                        string[] strArray = str.Split(separator);
+                        listaBicicletas.Add(new Bicicleta(listaBicicletas.Count + 1, strArray[0].Trim(), strArray[1].Trim(), strArray[2].Trim(), Convert.ToDouble(strArray[3]), Convert.ToDouble(strArray[4]), Convert.ToBoolean(strArray[5])));
+                        str = reader.ReadLine();
+                    }
+                }
+                listaLinhas = File.ReadAllLines(caminho).ToList<string>();
+            }
+            catch (Exception exception1) {
+                Console.WriteLine(exception1);
+            }
+        }
+
+        //Exibir lista de bikes
+        public static void ExibirLista() {
+            if (listaBicicletas.Count == 0) {
+                Console.WriteLine("ERRO: Não há contas para exibir.\n");
+            }
+            else {
+                int num = 0;
+                while (true) {
+                    if (num >= listaBicicletas.Count) {
+                        break;
+                    }
+                    Console.WriteLine(listaBicicletas[num].ToString());
+                    num++;
+                }
+            }
+        }
+
+        //Listar Bikes
+        public static void ListarBikes() {
+            if (listaBicicletas.Count == 0) {
+                Console.WriteLine(listaVaziaString);
+            }
+            else {
+                int minIndex = 0;
+                int maxIndex = 4;
+                int minPage = (maxIndex + 1) / 5;
+                int maxPage = Convert.ToInt32(Math.Round(Convert.ToDouble(listaBicicletas.Count) / 5) > 0 ? Math.Round(Convert.ToDouble(listaBicicletas.Count) + 1) : 1);
+                int index = 0;
+
+                do {
+
+                    switch (opcao4) {
+                        case 0:
+                            Console.WriteLine("\n===================================\n\tLista de Bicicletas\n===================================\n");
+                            //PÁGINA ATUAL + QUANT. PÁGINAS
+                            Console.WriteLine($"Página {minPage}/{maxPage}:\n");
+                            //EXIBIR BIKES
+                            for (int i = 0; i < listaBicicletas.Count; i++) {
+                                Console.Write($"[{listaBicicletas[i].Id}] - {listaBicicletas[i].Modelo} {listaBicicletas[i].Tamanho} ");
+                                if (listaBicicletas[i].Disponivel) {
+                                    Console.Write("(Disponível)");
+                                }
+                                else {
+                                    Console.Write("(Indisponível)");
+                                }
+                                Console.WriteLine();
+                            }
+                            //OPÇÕES DE INPUT
+                            Console.WriteLine("\n\nInsira o número correspondente à opção desejada: \n");
+                            Console.WriteLine("[1] Acessar informações de uma bicicleta pelo ID \n[2] Remover bicicleta da lista \n[4] Página anterior \n[5] Próxima página \n[6] Sair");
+                            opcao4 = InputInt("\n> ");
+                            break;
+                        case 1:
+                            //ACESSAR INFORMAÇÕES PELO ID
+                            opcao4_b = InputInt("\nInsira o ID da bicicleta: ");
+                            if (opcao4_b < 0 || opcao4_b > listaBicicletas.Count - 1) {
+                                Console.WriteLine(opcaoInvalidaString);
+                            }
+                            else {
+                                Console.WriteLine("\nID: " + listaBicicletas[opcao4_b].Id);
+                                Console.WriteLine("Modelo: " + listaBicicletas[opcao4_b].Modelo);
+                                Console.WriteLine("Tamanho: " + listaBicicletas[opcao4_b].Tamanho);
+                                Console.WriteLine("Cor: " + listaBicicletas[opcao4_b].Cor);
+                                Console.WriteLine("Aluguel: R$ " + String.Format("{0:0.00}", listaBicicletas[opcao4_b].ValAluguel));
+                                Console.WriteLine("Depósito: R$ " + String.Format("{0:0.00}", listaBicicletas[opcao4_b].ValDeposito));
+                                if (listaBicicletas[opcao4_b].Disponivel) {
+                                    Console.WriteLine("Disponível: sim");
+                                }
+                                else {
+                                    Console.WriteLine("Disponível: não");
+                                }
+                            }
+                            Thread.Sleep(2500);
+                            break;
+                        default:
+                            Console.WriteLine(opcaoInvalidaString);
+                            break;
+
+                    }
+                } while (true);
+            }
         }
     }
 }
